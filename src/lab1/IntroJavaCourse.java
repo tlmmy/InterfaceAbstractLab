358456deff6d9d3262e40b0e8b156dd6c8711b34
trogers8 <trogers8@L11702042765.wctc.msft>
2016-01-28 14:55:34 -0600

    moved prerequisites to super class

--- src/lab1/IntroJavaCourse.java
+++ src/lab1/IntroJavaCourse.java
@@ -15,19 +15,6 @@ public class IntroJavaCourse extends ProgrammingCourse {
         super(courseName, courseNumber);
     }
 
-       @Override
- public void setPrerequisites(String prerequisites) {
-        if(prerequisites == null || prerequisites.length() == 0) {
-            JOptionPane.showMessageDialog(null,
-                    "Error: prerequisites cannot be null of empty string");
-            System.exit(0);
-        }
-        this.prerequisites = prerequisites;
-    }
- 
- @Override
-    public String getPrerequisites() {
-        return prerequisites;
-    }
+     
 
 }
